5827f57152d6f6ba85699f4ebbc9da7a7f403248
Narfyx <kevin.abribat@live.fr>
2024-03-20 16:58:11 +0100

    python.py flake8 OK

--- python.py
+++ python.py
@@ -2,41 +2,44 @@ import os
 import json
 from control.control_main import MainControl
 
-#Constante
+# Constante
 DATABASE_DIRECTORY = "data"
 DIRECTORY_CONTENT = ("players", "tournaments")
 
-def create_directory_if_not_exists(directory_path:str, file:str):
+
+def create_directory_if_not_exists(directory_path: str, file: str):
     """
-    Creates a directory if it does not already exist and initializes an empty JSON file within it.
+    Creates a directory if it does not already exist and
+    initializes an empty JSON file within it.
 
     Args:
         directory_path (str): The path of the directory to be created.
-        file (str): The name of the file (including extension) to be created within the directory.
+        file (str): The name of the file (including extension)
+        to be created within the directory.
     """
-    directory_file = directory_path+'/'+file
+    directory_file = os.path.join(directory_path, file)
     if not os.path.exists(directory_file):
         os.makedirs(directory_file)
 
-        with open(f"{directory_file+'/'+file}.json", "w", encoding="utf-8") as outfile:
+        json_file_path = os.path.join(directory_file, f"{file}.json")
+        with open(json_file_path, "w", encoding="utf-8") as outfile:
             json.dump("", outfile)
 
 
 def main():
     """
-    Main function that ensures the existence of specified directories within the DATABASE_DIRECTORY.
+    Main function that ensures the existence of specified
+    directories within the DATABASE_DIRECTORY.
 
-    Iterates through the folders listed in DIRECTORY_CONTENT and calls the 
-    create_directory_if_not_exists function to create each folder within the DATABASE_DIRECTORY.
+    Iterates through the folders listed in DIRECTORY_CONTENT and calls the
+    create_directory_if_not_exists function to create each folder
+    within the DATABASE_DIRECTORY.
     """
     for folder in DIRECTORY_CONTENT:
         create_directory_if_not_exists(DATABASE_DIRECTORY, folder)
 
     MainControl()
-    
-
 
 
 if __name__ == '__main__':
     main()
-    
